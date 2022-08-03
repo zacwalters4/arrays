7e1eb1cce4c86bff8602dbc4ab012c0de04d9f24
Zac Walters <zacwalters4@gmail.com>
2022-08-03 09:00:49 -0600

    Add array methods and comments

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var strings = ["zac", "walters", "zacwalters4@gmail.com", "hello" ];
 var numbers = [0, 1, 2, 3];
 var booleans = [true, false, false, true];
+
+//Call and display the last(3) index position of var strings
+console.log(strings[3]);
+
+//Display a portion of the array using slice()
+console.log(numbers.slice(1, 3));
+
+//Remove and return the last element of the array with pop()
+console.log(booleans.pop());
